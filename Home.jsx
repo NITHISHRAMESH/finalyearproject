4b7fa20b87400aa11d156d690468353b31d79ec5
NITHISHKUMAR R <101456665+NITHISHRAMESH@users.noreply.github.com>
2022-06-25 20:19:25 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { Sidebar, UserProfile } from '../components';
+import { userQuery } from '../utils/data';
+import { client } from '../client';
+import Pins from './Pins';
+import logo from '../assets/logo.png';
+
+
+const Home = () => {
+  
+ const [toggleSidebar, setToggleSidebar] = useState(true);
+ const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+ const [user, setUser] = useState();
+ const scrollRef = useRef(null);
+
+ 
+ useEffect(() => {
+  const query = userQuery(userInfo?.googleId);
+
+  client.fetch(query).then((data) => {
+    setUser(data[0]);
+  });
+}, []);
+
+useEffect(() => {
+  scrollRef.current.scrollTo(0, 0);
+});
+
+    return(
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+      <Sidebar user={user && user} />
+      </div>
+      
+      <div className="flex md:hidden flex-row">
+      <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+      <Link to="/">
+      
+      <img src={logo} alt="logo" className="w-28" />
+      </Link>
+      </div>
+      {toggleSidebar && (
+        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+          <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+          </div>
+          <Sidebar closeToggle={setToggleSidebar} user={user && user} />
+        </div>
+        )}
+        <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Pins user={user && user} />} />
+        </Routes>
+      </div>
+
+
+      </div>
+  )
+}
+export default Home
---
+++ Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { IoMdAdd, IoMdSearch } from 'react-icons/io';
+import logo from '../assets/logopage.png';
+
+const Navbar = ({ searchTerm, setSearchTerm, user }) => {
+    const navigate = useNavigate();
+    //if(!user) return null;
+  return (
+    <div clasaName="flexgap-2 md:gap-5 w-full mt-5 pb-7">
+    <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm"> 
+    <IoMdSearch fontSize={21} className="ml-1" />
+    <input
+            type="text"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search"
+            value={searchTerm}
+            onFocus={() => navigate('/search')}
+            className="p-2 w-full bg-white outline-none"
+          />
+          </div><div classname="flex gap-3">
+          <Link to={'user-profile/${user?._id}'} className="hidden md:block">
+      
+      <img src={logo}  alt="user-pic" className="w-25 h-10 rounded-lg" />
+      </Link>
+          </div>
+        
+          </div>
+        
+         
+            );
+          }
+          
+          export default Navbar;
+          
